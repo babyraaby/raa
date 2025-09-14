bc2d867a8909fd02d5938a6656601a5e62164483
babyraaby <babyraaby223@gmail.com>
2025-09-14 18:36:39 +0700

    Add files via upload

---
+++ project zahra.js
@@ -0,0 +1,6 @@
+var yakin = confirm("Lanjut gak?");
+    if (yakin) {
+      alert('Silahkan mengisi untuk melihat cerita saya...');
+    } else {
+      window.location = "https://www.codedex.io/";
+    }
